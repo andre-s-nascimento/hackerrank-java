00c6215f61a448546507cc56adc7b6864c84dd79
André Soares Nascimento <andresoaresnascimento@gmail.com>
2021-03-07 12:58:59 -0300

    Dia 3 do 30 days of Code

---
+++ thirty_days_of_code/Day03.java
@@ -0,0 +1,36 @@
+package thirty_days_of_code;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Day03 {
+//https://www.hackerrank.com/challenges/30-conditional-statements
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if (N % 2 != 0) {
+            System.out.println("Weird");
+        } else {
+            if ((N >= 2) && (N <= 5)) {
+                System.out.println("Not Weird");
+            } else {
+                if ((N >= 6) && (N <= 20)) {
+                    System.out.println("Weird");
+                } else {
+                    System.out.println("Not Weird");
+                }
+            }
+        }
+
+        scanner.close();
+    }
+}
